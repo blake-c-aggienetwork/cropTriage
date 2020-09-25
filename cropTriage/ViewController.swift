5322b6565507ad5367c6b2bd822c6f3c9c128b30
blake <blkcrr939@gmail.com>
2020-09-25 10:49:48 -0500

    Initial Commit

---
+++ cropTriage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cropTriage
+//
+//  Created by Blake Carr on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
